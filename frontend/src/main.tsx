b04a4fad6ebf2419260aa2579d74a1d8bff775a2
Penumathsa sai manikanta varma <saimanikantavarma04@gmail.com>
2026-09-13 00:46:02 +0530

    fix: simplify frontend entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './store';
-import { RouterProvider, useRoutes } from 'react-router-dom';
-import routes from './routes';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider routes={routes} />
-  </React.StrictMode>
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode><App /></React.StrictMode>
 );
