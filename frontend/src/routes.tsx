edf815a4ee40dc56e572c95f82697401b31d223a
Penumathsa sai manikanta varma <saimanikantavarma04@gmail.com>
2026-09-13 00:39:18 +0530

    fix: correct page exports in frontend routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,23 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ProductListPage from './pages/ProductListPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrderSuccessPage from './pages/OrderSuccessPage';
-import OrderTrackingPage from './pages/OrderTrackingPage';
-import ProfilePage from './pages/ProfilePage';
-import AdminDashboardPage from './pages/admin/AdminDashboardPage';
-import AdminProductsPage from './pages/admin/AdminProductsPage';
-import AdminOrdersPage from './pages/admin/AdminOrdersPage';
-import LoginPage from './pages/Auth/LoginPage';
-import RegisterPage from './pages/Auth/RegisterPage';
+import { HomePage } from './pages/HomePage';
+import { ProductListPage } from './pages/ProductListPage';
+import { ProductDetailPage } from './pages/ProductDetailPage';
+import { CartPage } from './pages/CartPage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { OrderSuccessPage } from './pages/OrderSuccessPage';
+import { OrderTrackingPage } from './pages/OrderTrackingPage';
+import { ProfilePage } from './pages/ProfilePage';
+import { AdminDashboardPage } from './pages/admin/AdminDashboardPage';
+import { AdminProductsPage } from './pages/admin/AdminProductsPage';
+import { AdminOrdersPage } from './pages/admin/AdminOrdersPage';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductListPage />} />
       <Route path="/product/:id" element={<ProductDetailPage />} />
