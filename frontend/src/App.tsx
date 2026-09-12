c17743cfb68574c79459461a4ce0cce83066fb72
Penumathsa sai manikanta varma <saimanikantavarma04@gmail.com>
2026-09-13 00:45:58 +0530

    fix: correct App page exports and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,31 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navigation/Navbar';
-import HomePage from './pages/HomePage';
-import ProductListPage from './pages/ProductListPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrderSuccessPage from './pages/OrderSuccessPage';
-import OrderTrackingPage from './pages/OrderTrackingPage';
-import ProfilePage from './pages/ProfilePage';
-import AdminDashboardPage from './pages/admin/AdminDashboardPage';
-import AdminProductsPage from './pages/admin/AdminProductsPage';
-import AdminOrdersPage from './pages/admin/AdminOrdersPage';
+import { Navbar } from './components/navigation/Navbar';
+import { HomePage } from './pages/HomePage';
+import { ProductListPage } from './pages/ProductListPage';
+import { ProductDetailPage } from './pages/ProductDetailPage';
+import { CartPage } from './pages/CartPage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { OrderSuccessPage } from './pages/OrderSuccessPage';
+import { OrderTrackingPage } from './pages/OrderTrackingPage';
+import { ProfilePage } from './pages/ProfilePage';
+import { AdminDashboardPage } from './pages/admin/AdminDashboardPage';
+import { AdminProductsPage } from './pages/admin/AdminProductsPage';
+import { AdminOrdersPage } from './pages/admin/AdminOrdersPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <main className="min-h-screen bg-background p-4">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductListPage />} />
-          <Route path="/products/:id" element={<ProductDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/order/success" element={<OrderSuccessPage />} />
-          <Route path="/order/tracking" element={<OrderTrackingPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
-          <Route path="/admin/products" element={<AdminProductsPage />} />
-          <Route path="/admin/orders" element={<AdminOrdersPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <BrowserRouter><Navbar /><Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductListPage />} />
+    <Route path="/product/:id" element={<ProductDetailPage />} />
+    <Route path="/cart" element={<CartPage />} />
+    <Route path="/checkout" element={<CheckoutPage />} />
+    <Route path="/order/success" element={<OrderSuccessPage />} />
+    <Route path="/order/tracking" element={<OrderTrackingPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
+    <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+    <Route path="/admin/products" element={<AdminProductsPage />} />
+    <Route path="/admin/orders" element={<AdminOrdersPage />} />
+  </Routes></BrowserRouter>;
 }
 
 export default App;
