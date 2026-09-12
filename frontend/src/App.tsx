28d549c4fe202cb3f0ab85847c8a287b2d78f374
Penumathsa sai manikanta varma <saimanikantavarma04@gmail.com>
2026-09-12 19:49:30 +0530

    fix: import React Router route components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navigation/Navbar';
 import HomePage from './pages/HomePage';
 import ProductListPage from './pages/ProductListPage';
