ef6db923687b281a2d92dcd0fbe47b580772860d
Penumathsa sai manikanta varma <saimanikantavarma04@gmail.com>
2026-09-15 13:17:26 +0530

    feat: enable global accessibility styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './accessibility.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
